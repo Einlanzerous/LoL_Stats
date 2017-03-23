60fda67e4ee42ec7c5468519cd9912df0859c8be
mghuy <mghuy@unca.edu>
2017-03-23 16:53:33 -0400

    Add files via upload

--- API Project/index.js
+++ API Project/index.js
@@ -1,3 +1,8 @@
+/* @authors: Ashely Dodson, Leng Ghuy, Michael Scott
+   @class: CSCI 344 - Web Technology
+   @date: March 23, 2017
+*/
+
 (function () {
     "use strict";
     
@@ -8,32 +13,6 @@
         bodyParser = require('body-parser'),
         mongoose = require('mongoose'),
         app = express();
-    var assert = require('assert');
-    var ObjectId = require('mongodb').ObjectID;
-    var url = 'mongodb://ruler:Katarina7!@ds135700.mlab.com:35700/summonertest';
-
-    /*var pullnames = function(db, callback) {
-        var found = false;
-        var cursor = db.collection('summonerdata').find();
-        cursor.each(function(err, doc) {
-            assert.equal(err, null);
-            if ((doc != null) && !found) {
-                var test = doc;
-                if(test.summoner == "Einlanzerous"){
-                    //console.log(doc);
-                    var obj = {
-                        "kills": test.kills,
-                        "summoner": test.summoner
-                    }
-                    return obj;
-                    found = true;
-                }
-                //console.dir(doc);
-            } else {
-                callback();
-            }
-        });
-    };*/
     
     mongoose.Promise = global.Promise;
     
@@ -70,17 +49,6 @@
             "date_created": String,
             "matches_polled": Number
         }));
-    
-    /*app.get("/getStat", function(req, res) {
-        MongoClient.connect(url, function(err, db) {
-            assert.equal(null, err);
-            console.log(pullnames(db, function() {
-                db.close();
-            }));
-            //console.log(stat);
-            //res.json(stat);
-        });
-    });*/
     
     app.get("/getStat", function(req, res) {
         Stat.find(req.query, function(err, stat) {
